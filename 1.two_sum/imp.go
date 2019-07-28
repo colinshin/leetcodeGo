e196d65755f2610a86ac4bacc3e195e308deaa24
Rangding Zhang <love-nankai@163.com>
2019-07-28 19:38:18 +0800

    Create imp.go

---
+++ 1.two_sum/imp.go
@@ -0,0 +1,12 @@
+package __two_sum
+
+func twoSum(nums []int, target int) []int {
+	index := make(map[int]int, len(nums))
+	for i, element := range nums {
+		if j, found := index[target-element]; found {
+			return []int{j, i} // not {i, j}, but {j, i}; let's think, j < i
+		}
+		index[element] = i
+	}
+	return nil
+}
