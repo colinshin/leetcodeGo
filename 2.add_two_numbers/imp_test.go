57567f6559d5725907ba5fbb26fc9cc6fde043f3
Rangding Zhang <love-nankai@163.com>
2019-07-28 19:39:59 +0800

    Create imp_test.go

---
+++ 2.add_two_numbers/imp_test.go
@@ -0,0 +1,18 @@
+package __add_two_numbers
+
+import (
+	"testing"
+)
+
+func TestAddTwoNumbers(t *testing.T) {
+	n1 := NewWithArray([]int{2, 4, 3})
+	n2 := NewWithArray([]int{5, 6, 4})
+	r := addTwoNumbers(n1, n2)
+	t.Log(n1)
+	t.Log(n2)
+	t.Log(r)
+	expected := NewWithArray([]int{7, 0, 8})
+	if r.String() != expected.String() {
+		t.Error("expect:", expected, "got:", r)
+	}
+}
