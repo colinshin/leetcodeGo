28d154d6f56eea314a6d76688c4840c348736200
Rangding Zhang <love-nankai@163.com>
2019-07-29 12:46:27 +0800

    Update imp_test.go

--- 4.median_of_two_sorted_arrays/imp_test.go
+++ 4.median_of_two_sorted_arrays/imp_test.go
@@ -4,19 +4,20 @@ import (
 	"testing"
 )
 
-func TestFindMedianSortedArrays(t *testing.T)  {
-	cases := []struct{
-		a []int
-		b []int
+func TestFindMedianSortedArrays(t *testing.T) {
+	cases := []struct {
+		a      []int
+		b      []int
 		expect float64
 	}{
-		{a:[]int{1, 3}, b:[]int{2}, expect:2.0},
-		{a:[]int{1, 3}, b:[]int{2, 4}, expect:2.5},
-		{a:[]int{1, 3}, b:[]int{2, 4, 8}, expect:3.0},
+		{a: []int{1, 3}, b: []int{2}, expect: 2.0},
+		{a: []int{1, 3}, b: []int{2, 4}, expect: 2.5},
+		{a: []int{1, 3}, b: []int{2, 4, 8}, expect: 3.0},
+		{a: []int{1, 3, 5}, b: []int{8, 10}, expect: 5.0},
 	}
 	for _, c := range cases {
 		r := findMedianSortedArrays(c.a, c.b)
-		if c.expect - r > 0.0000001 {
+		if c.expect-r > 0.0000001 {
 			t.Error("expect:", c.expect, "got:", r)
 		}
 	}
