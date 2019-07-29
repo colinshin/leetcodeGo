559378baf2575bc1ef5418d390b82db957e9927c
Rangding Zhang <love-nankai@163.com>
2019-07-29 12:45:52 +0800

    Update imp.go

--- 4.median_of_two_sorted_arrays/imp.go
+++ 4.median_of_two_sorted_arrays/imp.go
@@ -1,25 +1,46 @@
 package __median_of_two_sorted_arrays
 
-//func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-//	return (medianOfArray(nums1) + medianOfArray(nums2)) * 0.5
-//}
+import "github.com/zrcoder/leetcodeGo/util/integer"
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	l1, l2 := len(nums1), len(nums2)
-	if l1 < l2 {
-		l1, l2 = l2, l1
-		nums1, nums2 = nums1, nums2
+	m, n := len(nums1), len(nums2)
+	if m > n {
+		m, n = n, m
+		nums1, nums2 = nums2, nums1
 	}
-	return (medianOfArray(nums1) + medianOfArray(nums2)) * 0.5
-}
 
-func medianOfArray(nums []int) float64 {
-	length := len(nums)
-	if length == 0 {
-		return 0.0
+	iMin, iMax, halfLen := 0, m, (m+n+1)/2
+	for iMin <= iMax {
+		i := (iMin + iMax) / 2
+		j := halfLen - i
+		if i < iMax && nums2[j-1] > nums1[i] {
+			iMin = i + 1
+		} else if i > iMin && nums1[i-1] > nums2[j] {
+			iMax = i - 1
+		} else {
+			maxLeft := 0
+			if i == 0 {
+				maxLeft = nums2[j-1]
+			} else if j == 0 {
+				maxLeft = nums1[i-1]
+			} else {
+				maxLeft = integer.Max(nums1[i-1], nums2[j-1])
+			}
+
+			if (m+n)%2 == 1 {
+				return float64(maxLeft)
+			}
+
+			minRight := 0
+			if i == m {
+				minRight = nums2[j]
+			} else if j == n {
+				minRight = nums1[i]
+			} else {
+				minRight = integer.Min(nums1[i], nums2[j])
+			}
+			return float64(maxLeft+minRight) / 2
+		}
 	}
-	if length%2 == 0 {
-		return float64(nums[length/2] + nums[length/2-1]) / 2
-	}
-	return float64(nums[length/2])
+	return 0.0
 }
