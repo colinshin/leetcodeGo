3f2a3605505d75012eec416b9880b1738354fc66
Rangding Zhang <love-nankai@163.com>
2019-12-07 14:31:04 +0800

    *

--- 3.longest_substring_without_repeating_characters/imp.go
+++ 3.longest_substring_without_repeating_characters/imp.go
@@ -1,25 +1,17 @@
 package __longest_substring_without_repeating_characters
 
-import "testing"
+import "github.com/zrcoder/leetcodeGo/util/integer"
 
-func TestLengthOfLongestSubstring(t *testing.T) {
-	cases := []struct {
-		input  string
-		expect int
-	}{
-		{input: "abcabcbb", expect: 3},
-		{input: "bbbbb", expect: 1},
-		{input: "pwwkew", expect: 3},
-		{input: "abcdefg123456789999", expect: 16},
-		{input: "abcadefg123a456789999", expect: 14},
-		{input: "", expect: 0},
-		{input: "x", expect: 1},
-	}
-
-	for _, c := range cases {
-		r := lengthOfLongestSubstring(c.input)
-		if c.expect != r {
-			t.Error("input:", c.input, "expect:", c.expect, "got:", r)
+func lengthOfLongestSubstring(s string) int {
+	m := map[interface{}]int{}
+	max := 0
+	for left, right := 0, 0; right < len(s); right ++ {
+		c := s[right]
+		if index, found := m[c]; found {
+			left = integer.Max(left, index+1)
 		}
+		max = integer.Max(max, right-left+1)
+		m[c] = right
 	}
+	return max
 }
