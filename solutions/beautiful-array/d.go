ec11fd62c4836dc35534f17c0e9c42f0e8af454f
zrcoder <love-nankai@163.com>
2020-02-26 15:20:33 +0800

    *

--- solutions/beautiful-array/d.go
+++ solutions/beautiful-array/d.go
@@ -6,23 +6,15 @@ package beautiful_array
 
 /*
 对于某些固定的 N，如果数组 A 是整数 1, 2, ..., N 组成的排列，使得：
-
 对于每个 i < j，都不存在 k 满足 i < k < j 使得 A[k] * 2 = A[i] + A[j]。
-
 那么数组 A 是漂亮数组。
-
-
-
 给定 N，返回任意漂亮数组 A（保证存在一个）。
 
-
-
 示例 1：
-
 输入：4
 输出：[2,1,4,3]
-示例 2：
 
+示例 2：
 输入：5
 输出：[3,1,2,5,4]
 
@@ -30,95 +22,11 @@ package beautiful_array
 链接：https://leetcode-cn.com/problems/beautiful-array
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
-
 /*
-漂亮数组有以下性质:
+官方题解： 自顶向下递归
 
-（1）A是一个漂亮数组，如果对A中所有元素添加一个常数，那么Ａ还是一个漂亮数组。
-
-（2）A是一个漂亮数组，如果对A中所有元素乘以一个常数，那么A还是一个漂亮数组。
-
-（3）A是一个漂亮数组，如果删除一些A中所有元素，那么A还是一个漂亮数组。
-
-（4) A是一个奇数构成的漂亮数组，B是一个偶数构成的漂亮数组，那么A+B也是一个漂亮数组
-比如:{1,5,3,7}+{2,6,4,8}={1,5,3,7,2,6,4,8}也是一个漂亮数组。
-
-所以我们假设一个{1,...,m}的数组是漂亮数组，可以通过下面的方式将其规模翻倍，构造漂亮数组{1,...,2m}:
-
-对{1,...,m}中所有的数*2并-1，构成一个奇数漂亮数组A。如{1,3,2,4}，变换为{1,5,3,7}
-补齐偶数数组,如上面的{1,5,3,7}, 每个元素+1得到{2,6,4,8}
-A+B构成了漂亮数组{1,...,2m}。这个例子里：{1,5,3,7}+{2,6,4,8}={1,5,3,7,2,6,4,8}
-
-因每次翻倍构造，会有写多余的元素（大于N），最后需要剔除这些元素
-
-作者：zerorlis-2
-链接：https://leetcode-cn.com/problems/beautiful-array/solution/piao-liang-shu-zu-de-yi-xie-xing-zhi-bing-qie-ji-y/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-*/
-func beautifulArray0(N int) []int {
-	switch N {
-	// 比较小的数字直接返回
-	case 1:
-		return []int{1}
-	case 2:
-		return []int{1, 2}
-	case 3:
-		return []int{1, 3, 2}
-	case 4:
-		return []int{1, 3, 2, 4}
-	}
-	// 从{1,3,2,4}，4个数字开始构建漂亮数组，当然从{1}开始构建也可以
-	r := []int{1, 3, 2, 4}
-	for len(r) < N {
-		n := len(r)
-		// 得到左侧奇数漂亮数组
-		for i := 0; i < n; i++ {
-			r[i] = 2*r[i] - 1
-		}
-		// 得到右侧偶数漂亮数组
-		for i := 0; i < n; i++ {
-			r = append(r, r[i]+1)
-		}
-	}
-	// 剔除多余元素
-	var rr []int
-	for _, v := range r {
-		if v <= N {
-			rr = append(rr, v)
-		}
-	}
-	return rr
-}
-
-func beautifulArray(N int) []int {
-	r := []int{1}
-	for len(r) < N {
-		n := len(r)
-		// 得到左侧奇数漂亮数组
-		for i := 0; i < n; i++ {
-			r[i] = 2*r[i] - 1
-		}
-		// 得到右侧偶数漂亮数组
-		for i := 0; i < n; i++ {
-			r = append(r, r[i]+1)
-		}
-	}
-	// 剔除多余元素
-	var rr []int
-	for _, v := range r {
-		if v <= N {
-			rr = append(rr, v)
-		}
-	}
-	return rr
-}
-
-/*
-官方题解：
-
-首先我们可以发现一个不错的性质，如果某个数组 [a_1, a_2, \cdots, a_n][a1,a2,⋯,an] 是漂亮的，
-那么对这个数组进行仿射变换，得到的新数组 [ka_1+b, ka_2+b, \cdots, ka_n+b][ka1+b,ka2+b,⋯,kan+b] 也是漂亮的（其中 k ！= 0）。
+首先我们可以发现一个不错的性质，如果某个数组[a1,a2,⋯,an] 是漂亮的，
+那么对这个数组进行仿射变换，得到的新数组[ka1+b,ka2+b,⋯,kan+b] 也是漂亮的（其中 k ！= 0）。
 那么我们就有了一个想法：将数组分成两部分 left 和 right，分别求出一个漂亮的数组，然后将它们进行仿射变换，使得不存在满足下面条件的三元组：
 A[k] * 2 = A[i] + A[j], i < k < j；
 A[i] 来自 left 部分，A[j] 来自 right 部分。
@@ -139,11 +47,13 @@ A[i] 来自 left 部分，A[j] 来自 right 部分。
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 */
-func beautifulArray1(N int) []int {
+func beautifulArray(N int) []int {
+	if N < 1 {
+		return nil
+	}
 	if N == 1 {
 		return []int{1}
 	}
-
 	var r []int
 	for _, v := range beautifulArray((N + 1) / 2) { // 奇数们
 		r = append(r, 2*v-1)
@@ -155,25 +65,111 @@ func beautifulArray1(N int) []int {
 }
 
 /*
-可进一步优化，用一个map储存计算结果
+可用一个map储存计算结果，优化递归栈及时间
 */
-var m = make(map[int][]int, 0)
-
 func beautifulArray2(N int) []int {
-	if N == 1 {
-		m[N] = []int{1}
+	if N < 1 {
+		return nil
 	}
-	if v, ok := m[N]; ok {
-		return v
+	m := make(map[int][]int, 0) // 缓存中间计算结果，减少递归
+	var helper func(n int) []int
+	helper = func(n int) []int {
+		if n == 1 {
+			return []int{1}
+		}
+		if v, ok := m[n]; ok {
+			return v
+		}
+		var r []int
+		for _, v := range helper((n + 1) / 2) { // 奇数们
+			r = append(r, 2*v-1)
+		}
+		for _, v := range helper(n / 2) { // 偶数们
+			r = append(r, 2*v)
+		}
+		m[n] = r
+		return r
 	}
+	return helper(N)
+}
 
-	var r []int
-	for _, v := range beautifulArray((N + 1) / 2) { // 奇数们
-		r = append(r, 2*v-1)
+/*
+自底向上，循环
+
+漂亮数组有以下性质:
+（1）A是一个漂亮数组，如果对A中所有元素添加一个常数，那么Ａ还是一个漂亮数组。
+（2）A是一个漂亮数组，如果对A中所有元素乘以一个常数，那么A还是一个漂亮数组。
+（3）A是一个漂亮数组，如果删除一些A中元素，那么A还是一个漂亮数组。
+（4) A是一个奇数构成的漂亮数组，B是一个偶数构成的漂亮数组，那么A+B也是一个漂亮数组
+比如:{1,5,3,7}+{2,6,4,8}={1,5,3,7,2,6,4,8}也是一个漂亮数组。
+
+所以我们假设一个{1,...,m}的数组是漂亮数组，可以通过下面的方式将其规模翻倍，构造漂亮数组{1,...,2m}:
+对{1,...,m}中所有的数*2并-1，构成一个奇数漂亮数组A。如{1,3,2,4}，变换为{1,5,3,7}
+补齐偶数数组,如上面的{1,5,3,7}, 每个元素+1得到{2,6,4,8}
+A+B构成了漂亮数组{1,...,2m}。这个例子里：{1,5,3,7}+{2,6,4,8}={1,5,3,7,2,6,4,8}
+因每次翻倍构造，会有些多余的元素（大于N），最后需要剔除这些元素
+在N比较小时的构造结果如下：
+N == 1：{1};
+N == 2：{1, 2};
+N == 3：{1, 3, 2, 4} -> {1, 3, 2};  元素4剔除
+N == 4：{1, 5, 3, 2, 6, 4} -> {1, 3, 2, 4};  5, 6被剔除
+
+作者：zerorlis-2
+链接：https://leetcode-cn.com/problems/beautiful-array/solution/piao-liang-shu-zu-de-yi-xie-xing-zhi-bing-qie-ji-y/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+func beautifulArray0(N int) []int {
+	if N < 1 {
+		return nil
 	}
-	for _, v := range beautifulArray(N / 2) { // 偶数们
-		r = append(r, 2*v)
+	r := []int{1}
+	for len(r) < N {
+		n := len(r)
+		for i := 0; i < n; i++ { // 得到左侧奇数漂亮数组
+			r[i] = 2*r[i] - 1
+		}
+		for i := 0; i < n; i++ { // 得到右侧偶数漂亮数组
+			r = append(r, r[i]+1)
+		}
 	}
-	m[N] = r
-	return r
+	var result []int
+	for _, v := range r {
+		if v <= N { // 剔除多余， 只保留在N之内的元素
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+// 可以精确控制上边实现里的r和result的大小
+func beautifulArray1(N int) []int {
+	if N < 1 {
+		return nil
+	}
+	r := make([]int, 2*N-1)
+	r[0] = 1
+	length := 1
+	for length < N {
+		for i := 0; i < length; i++ {
+			r[i] = 2*r[i] - 1      // 得到左侧奇数漂亮数组
+			r[i+length] = r[i] + 1 // 得到右侧偶数漂亮数组
+		}
+		length *= 2
+	}
+	if length == N {
+		return r[:N]
+	}
+	result := make([]int, N)
+	k := 0
+	for _, v := range r {
+		if k == N {
+			break
+		}
+		if v <= N { // 剔除多余， 只保留在N之内的元素
+			result[k] = v
+			k++
+		}
+	}
+	return result
 }
--- solutions/linked-list-components/d.go
+++ solutions/linked-list-components/d.go
@@ -37,26 +37,51 @@ G = [0, 3, 1, 4]
 链接：https://leetcode-cn.com/problems/linked-list-components
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
+/*
+遍历链表，通过查看链表节点的值是否在G中来统计
+用一个临时变量表示上一个节点的值是否在G中，这样就能正确统计
+判断元素是否在数组G中比较耗时，可以将G里的元素放入哈希表来优化
 
+假设链表和列表的长度分别为m，n，则时间复杂度是O(m+n)，空间复杂度是O(n)
+*/
 func numComponents(head *ListNode, G []int) int {
-	if len(G) == 0 || head == nil {
+	if head == nil || len(G) == 0 {
 		return 0
 	}
-
 	set := make(map[int]struct{}, len(G))
 	for _, v := range G {
 		set[v] = struct{}{}
 	}
+	count := 0
+	isPrevNodeInG := false
+	for head != nil {
+		_, isCurrentNodeInG := set[head.Val]
+		if isCurrentNodeInG && !isPrevNodeInG {
+			count++
+		}
+		isPrevNodeInG = isCurrentNodeInG
+		head = head.Next
+	}
+	return count
+}
 
+// 也可以在遍历链表的时候，同时看当前节点和下一个节点是否在G中来统计
+func numComponents1(head *ListNode, G []int) int {
+	if len(G) == 0 || head == nil {
+		return 0
+	}
+	set := make(map[int]struct{}, len(G))
+	for _, v := range G {
+		set[v] = struct{}{}
+	}
 	count := 0
 	for p := head; p != nil; p = p.Next {
-		_, isCurrentNodeInSet := set[p.Val]
-		isNextNodeInSet := false
+		_, isCurrentNodeInG := set[p.Val]
+		isNextNodeInG := false
 		if p.Next != nil {
-			_, isNextNodeInSet = set[p.Next.Val]
+			_, isNextNodeInG = set[p.Next.Val]
 		}
-
-		if isCurrentNodeInSet && !isNextNodeInSet {
+		if isCurrentNodeInG && !isNextNodeInG {
 			count++
 		}
 	}
