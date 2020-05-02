838f947d7518698c9d5a00ae504033e25d05b691
Rangding Zhang <love-nankai@163.com>
2020-05-02 10:52:45 +0800

    *

--- solutions/longest-substring-without-repeating-characters/d.go
+++ solutions/longest-substring-without-repeating-characters/d.go
@@ -7,28 +7,25 @@ package longest_substring_without_repeating_characters
 import "math"
 
 /*
+3. 无重复字符的最长子串 https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+
 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
 
 示例 1:
-
 输入: "abcabcbb"
 输出: 3
 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
-示例 2:
 
+示例 2:
 输入: "bbbbb"
 输出: 1
 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
-示例 3:
 
+示例 3:
 输入: "pwwkew"
 输出: 3
 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 /*
 滑动窗口
@@ -94,33 +91,6 @@ func lengthOfLongestSubstring10(s string) int {
 		if r-l > result {
 			result = r - l
 		}
-	}
-	return result
-}
-
-/* 不通过，因假设前提不成立；但是思路可以看一下
-
-假设字符集为 ASCII 128,以上方法里的map可以改成数组
-以前的我们都没有对字符串 s 所使用的字符集进行假设
-当我们知道该字符集比较小的时侯，我们可以用一个整数数组作为直接访问表来替换 Map
-常用的表如下所示：
-int [26] 用于字母 ‘a’ - ‘z’ 或 ‘A’ - ‘Z’
-int [128] 用于ASCII码
-int [256] 用于扩展ASCII码
-*/
-func lengthOfLongestSubstring2(s string) int {
-	const charNum = 128
-	index := make([]int, charNum)
-	result := 0
-	for l, r := 0, 0; r < len(s); r++ {
-		c := s[r]
-		if index[c] > l {
-			l = index[c]
-		}
-		if r-l+1 > result {
-			result = r - 1 + l
-		}
-		index[c] = r + 1
 	}
 	return result
 }
